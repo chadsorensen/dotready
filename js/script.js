08a395ad5c93c80134e78299e013d52a8d810bf9
Chad Sorensen <chdsorensen@gmail.com>
2011-09-08 14:52:18 -0700

    Sorted out thumb nav functionality

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
  $(document).ready(function() {
-   $('.left_overlay').delay(500).animate({ width: "3%"}, 1500 );
-   $('.right_overlay').delay(500).animate({ width: "3%"}, 1500 );
+  // $('.left_overlay').delay(500).animate({ width: "3%"}, 1500 );
+  // $('.right_overlay').delay(500).animate({ width: "3%"}, 1500 );
 
-   $('.thumb').hover(
+   $('.thumb').click( function() { 
+       $project = $(this).attr('project');       
+       $('.thumb').removeClass('active');
+       $(this).addClass('active');
+       $('.project_wrap').stop(true,true).fadeOut(900, function() {
+         $($project).fadeIn(900);
+       });
+   });
+
+  $('.thumb').hover(
      function() {
-       $(this).find('.caption').stop(false,true).fadeIn(200);
-     },
-    function () {
        $(this).find('.caption').stop(false,true).fadeOut(200);
-    }     
+     }, 
+     function () {
+       $(this).find('.caption').stop(false,true).fadeIn(200);
+     }     
    );
-   
+
  });
